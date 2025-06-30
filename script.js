fb3112e506e124c068b564d2a3583adeaf18f81a
Ashwith D <136821981+AshwithD@users.noreply.github.com>
2025-06-30 11:53:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+ document.getElementById("hireBtn").addEventListener("click", function() {
+      alert("Thank you for your interest! Let's connect via email: ashwithd40@gmail.com");
+    });
+
+    document.getElementById("talkBtn").addEventListener("click", function() {
+      alert("Let's talk! Drop me a message on LinkedIn.");
+    });
